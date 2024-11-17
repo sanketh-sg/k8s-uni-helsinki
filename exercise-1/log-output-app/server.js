33e6420081f493e37950102470161f7071530dc0
Sanketh S G <sankethsg2944@gmail.com>
2024-11-17 12:59:51 +0100

    Added endpoint to show time and status

--- exercise-1/log-output-app/server.js
+++ exercise-1/log-output-app/server.js
@@ -10,21 +10,27 @@ const PORT = process.env.PORT || 3000;
 // Function to output the random string with a timestamp
 function outputRandomString() {
   const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}]: ${randomString}`);
+  let log = `[${timestamp}]: ${randomString}`;
+  console.log(log);
+  return log;
 }
 
 // Output the random string every 5 seconds
 setInterval(outputRandomString, 5000);
 
-// Initial output on startup
-outputRandomString();
+// // Initial output on startup
+// let status = outputRandomString();
 
 // API endpoint to handle GET request from the frontend
 app.get('/api/hi', async (req, res) => {
  res.send("Hello")
 });
 
+app.get('/api/status', async (req, res) => {
+  res.send(outputRandomString());
+ });
+
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Frontend viewer running on http://localhost:${PORT}/api/hi`);
+  console.log(`Frontend viewer running on http://localhost:${PORT}`);
 });
