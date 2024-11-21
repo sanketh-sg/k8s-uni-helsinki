65721484ea17220d696192ff53729d8e54a49a44
Sanketh S G <sankethsg2944@gmail.com>
2024-11-21 15:50:31 +0100

    Enhance log output app with image fetching functionality; tried to add NFS configuration and cron job for shared data management  it wont work: made sure the pods are always scheduled together by adding podAffinity

--- exercise-1/log-output-app/README.md
+++ exercise-1/log-output-app/README.md
@@ -9,3 +9,18 @@ Persistant volumes is cluster wide resource. Its is backed by variuos types of s
 Lifecycle is independent of pods. 
 
 The data will persist in the kind node container at /mnt/data 
+
+/mnt/data is the path on the host machine where your data is stored.
+
+/app/shared is the path inside the pod where the host directory (/mnt/data) is mounted.
+
+Always interact with the volume through the /app/shared path inside the container, not /mnt/data (which is only accessible from the host).
+
+hostPath mounts a volume on the node and application running on that node can only access that path.
+
+If you want to share files across nodes then we need to use nfs
+
+Well setting up nfs requires a nfs server which I dont have now and then run a dynamic provisioner on it to get a pv. 
+
+This cronJob idea was not suitable at this stage but it was great learning.
+
--- exercise-1/log-output-app/hash-reader.js
+++ exercise-1/log-output-app/hash-reader.js
@@ -5,6 +5,7 @@ const express = require('express');
 
 const logfilePath = path.join('/app/shared','timestamp.txt');
 const pingfilePath = path.join('/app/shared','pingpong-count.txt');
+const imageDir = path.dirname('/app/images')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -23,6 +24,8 @@ function readTimestamp() {
   }
 }
 
+
+//Routes
 app.get('/api/status', (req, res) => {
     let { timestamp, hash, count, error } = readTimestamp();
     
@@ -32,6 +35,16 @@ app.get('/api/status', (req, res) => {
     res.send(`${timestamp}: ${hash}<br>Ping / Pongs: ${count}`);
     }
 });
+
+app.get('/', (req, res) => {
+    const image = fs.readdirSync(imageDir);
+    const latestImage = image.sort().pop(); // Get the latest file based on timestamp
+    if (latestImage) {
+      res.sendFile(path.join(imageDir, latestImage));
+    } else {
+      res.status(404).send('No images found');
+    }
+  });
 
 app.listen(PORT, () => {
   console.log(`Log reader running on http://localhost:${PORT}`);
